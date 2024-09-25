0e4f71d8fcf87ba63588a2d201862c5cb75d3a77
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-09-25 21:56:45 +0100

    deployment

--- tutorials/Tuto deployment/tuto_8.php
+++ tutorials/Tuto deployment/tuto_8.php
@@ -1,3 +1,3 @@
 <?php
-// tuto link: "https://www.youtube.com/watch?v=3EAj9tsXLFU"
-echo "hello world";
+// tuto link: "https://chatgpt.com/c/66f420cb-110c-8006-8393-519d1ffb3868"
+echo "hello suirita fahd";
