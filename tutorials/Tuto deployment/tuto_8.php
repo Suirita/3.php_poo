86f94acb153208a809f1fe7997c2a29aa2e3ca29
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-09-26 10:00:15 +0100

    change deployment tuto link

--- tutorials/Tuto deployment/tuto_8.php
+++ tutorials/Tuto deployment/tuto_8.php
@@ -1,3 +1,3 @@
 <?php
-// tuto link: "hhttps://chatgpt.com/share/66f5168e-7054-8006-be6c-1b596e302fe6"
+// tuto link: "https://chatgpt.com/share/66f5168e-7054-8006-be6c-1b596e302fe6"
 echo "hello suirita fahd";
